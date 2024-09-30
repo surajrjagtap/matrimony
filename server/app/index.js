b631107e24f9cffa31fef137c63962648014d120
surajllpl <srj.llpl@gmail.com>
2024-09-30 17:22:16 +0530

    frontend setup

--- server/app/index.js
+++ server/app/index.js
@@ -1,12 +1,13 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-console.log(process.env.PORT) 
+  res.send('Hello World!');
+});
+
+console.log(`Using PORT: ${process.env.PORT}`);
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
